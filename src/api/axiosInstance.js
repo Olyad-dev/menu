6b32be2413c7d2c7097969557fabe765345a7c84
Olyad-dev <olayidgetinet@gmail.com>
2026-06-22 19:25:18 +0300

    test for login errors

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "/",
+  baseURL: "https://digital-menu-backend-y0dj.onrender.com/", 
   withCredentials: true,
 });
 
